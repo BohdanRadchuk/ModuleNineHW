14af7b213a4c600ac24616e4114f7ee9f670c23d
BohdanRadchuk <shpionuse@gmail.com>
2017-11-27 09:46:56 +0200

    1

--- src/main/java/YouTubeApi.java
+++ src/main/java/YouTubeApi.java
@@ -5,8 +5,11 @@ import objectsStructure.YouTubeMainResponce;
 public class YouTubeApi {
     private final static String YOUTUBE_ADRESS = "https://www.googleapis.com/youtube/v3/activities";
     private final static String MY_API_KEY  = "AIzaSyCl0nW-_pC68yOaRAvktYuByEKxYITPYNk";
-    public static HttpResponse<YouTubeMainResponce> youTubeApiWork(){
-        Unirest.get(YOUTUBE_ADRESS).routeParam()
+    public static HttpResponse<YouTubeMainResponce> youTubeApiWork(String channelID, int maxResults){
+                    Unirest.get(YOUTUBE_ADRESS)
+                            .queryString("channelId" , channelID)
+                    .queryString("maxResults", maxResults)
+                    .queryString();
         return null;
 
     }
